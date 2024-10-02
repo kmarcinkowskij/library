d26a0b81fa6a5251ac5e0484d0defc1e0ad45d4c
Konrad Marcinkowski <km.dartmoor@gmail.com>
2024-10-02 11:35:15 +0200

    code cleanup

--- utils/Utilities.cpp
+++ utils/Utilities.cpp
@@ -60,35 +60,6 @@ void Utilities::clear_file(const std::string &_file_dir) {
     std::rename("temp.csv", _file_dir.c_str());
 }
 
-int32_t Utilities::find_book_with_ULID(const std::string &v_ulid) {
-    std::ifstream input("books.csv");
-    std::string line;
-    std::array<std::string, 8>book_data;
-    std::tuple<std::string, int16_t>data;
-    int16_t line_iter = 0;
-    int32_t line_counter = 0;
-    int32_t counter = 0;
-    while(std::getline(input, line)) {
-        for(int i = 0; i < 8; i++) {
-            std::tuple<std::string, int16_t> data = Utilities::get_string_from_index_until_delimiter(line, counter, ',');
-            book_data[i] = std::get<0>(data);
-            counter = std::get<1>(data)+(int16_t)1;
-
-        }
-        if(book_data[1] == v_ulid) {
-            input.close();
-            return line_counter;
-        }
-
-        line_counter++;
-        counter = 0;
-    }
-    input.close();
-    return -1;
-}
-
-
-
 std::tuple<std::string, int16_t> Utilities::
 get_string_from_index_until_delimiter(const std::string &_string, const int16_t &index, const char &_delimiter) {
     std::string final;
